27f2d6bd04324e5d14dc7456cd7787d6a211bcc2
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2023-08-31 02:44:21 +0530

    Login page sample design

--- mobile/functionalcom.js
+++ mobile/functionalcom.js
@@ -2,11 +2,13 @@ import React from "react";
 import Button from '@ant-design/react-native/lib/button';
 import Text from '@ant-design/react-native/lib/text';
 import View from '@ant-design/react-native/lib/view';
+import TextInput from'@ant-design/react-native/lib/text';
+import  InputItem  from'@ant-design/react-native/lib/input-item';
 const functionalcom =()=>{
     return(
-<View>
-<Text>hello functional component</Text>
-<Button>Clik me</Button>
+<View  style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', paddingTop: 20,backgroundColor:'#E1E2EF' }}>
+<Text style={{justifyContent:'center',marginRight:30,fontSize: 40,color:'#05204A',}}> Login Page</Text>
+
 </View>
     );
 };
