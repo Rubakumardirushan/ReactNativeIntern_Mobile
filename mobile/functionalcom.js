e0494232601f6163c6c2a6a84e08716708eda029
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2023-08-31 02:05:34 +0530

    Create Basic Functional Component

---
+++ mobile/functionalcom.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from '@ant-design/react-native/lib/button';
+import Text from '@ant-design/react-native/lib/text';
+import View from '@ant-design/react-native/lib/view';
+const functionalcom =()=>{
+    return(
+<View>
+<Text>hello functional component</Text>
+<Button>Clik me</Button>
+</View>
+    );
+};
+export default functionalcom;
